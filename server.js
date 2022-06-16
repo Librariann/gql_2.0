0f1fdbdf65b954a709c2cb7eea5f4d6420b49ca9
Librariann <okpc0306@naver.com>
2022-06-16 19:43:49 +0900

    ~#4.7 Mutation Resolvers

--- server.js
+++ server.js
@@ -1,30 +1,73 @@
-import {ApolloServer, gql} from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
+
+let tweets = [
+  {
+    id: "1",
+    text: "first hello",
+  },
+  {
+    id: "2",
+    text: "second hello",
+  },
+];
 
 const typeDefs = gql`
   type User {
-    id:ID!
-    username:String!
+    id: ID!
+    username: String!
   }
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
-    allTweets: [Tweet]!
+    allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text:String, userId:ID): Tweet
-    deleteTweet(id:ID): Boolean
+    postTweet(text: String, userId: ID): Tweet
+    deleteTweet(id: ID): Boolean
   }
-`;tooitole
+`;
 //Query -> Get.. 데이터만 받아볼수있음..
 //Mutation -> Post, Put, Delete를 할때.. -> 데이터 수정이 있을경우!
-//'!'가 없으면 null 가능 '!'가 붙으면 Null이 올경우 에러 
+//'!'가 없으면 null 가능 '!'가 붙으면 Null이 올경우 에러
+//list -> []안에 넣으면됨
 
-const server = new ApolloServer({typeDefs})
+//Query안에 Post, Put, Delete같은 기능을 넣어도 되지만
+//Mutation으로 나누는 이유는 개념적으로 나눠놓고 코드를 더 잘 작성하기 위함이다.(유지보수 측면)
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    //args 순서가 중요하다
+    //_ -> root args, 두번째는 받아올 args
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+};
 
-server.listen().then(({url}) => {
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
-})
+});
