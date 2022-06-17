70f0db8c3b8fc2e8edb77e98ef2a3782888972ce
Librariann <okpc0306@naver.com>
2022-06-17 18:13:55 +0900

    end lecture

--- server.js
+++ server.js
@@ -4,24 +4,48 @@ let tweets = [
   {
     id: "1",
     text: "first hello",
+    userId: "2",
   },
   {
     id: "2",
     text: "second hello",
+    userId: "1",
+  },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "nico",
+    lastName: "las",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "musk",
   },
 ];
 
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
+    firstName: String!
+    lastName: String!
+    """
+    full name documentation
+    """
+    fullName: String!
   }
+  """
+  tweet object document test
+  """
   type Tweet {
     id: ID!
     text: String!
     author: User
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
   }
@@ -45,23 +69,39 @@ const resolvers = {
     tweet(root, { id }) {
       return tweets.find((tweet) => tweet.id === id);
     },
+    allUsers() {
+      return users;
+    },
   },
   Mutation: {
     //args 순서가 중요하다
-    //_ -> root args, 두번째는 받아올 args
-    postTweet(_, { text, userId }) {
+    //__ -> root args, 두번째는 받아올 args
+    postTweet(__, { text, userId }) {
+      const checkUserId = users.find((user) => user.id === userId);
+      if (!checkUserId) return false;
       const newTweet = {
         id: tweets.length + 1,
         text,
+        userId,
       };
       tweets.push(newTweet);
       return newTweet;
     },
-    deleteTweet(_, { id }) {
+    deleteTweet(__, { id }) {
       const tweet = tweets.find((tweet) => tweet.id === id);
       if (!tweet) return false;
       tweets = tweets.filter((tweet) => tweet.id !== id);
       return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
     },
   },
 };
